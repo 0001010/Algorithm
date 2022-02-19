cd5038da691ee6192b3e06d3511b7ce5dabcc28b
0001010 <icearrows_@naver.com>
2022-02-19 14:12:23 +0900

    Create 15965.py

---
+++ BOJ/15965.py
@@ -0,0 +1,12 @@
+# 15965
+def prime(n):
+    s = [True]*(n+1)
+    m = int(n**0.5)
+    for i in range(2, m+1):
+        if s[i]==True:
+            for j in range(i+i, n+1, i):
+                s[j]=False
+    return [i for i in range(2, n+1) if s[i]==True]
+
+k = int(input())
+print(prime(7500000)[k-1])
